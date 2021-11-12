d49cb9936602693ba018e77aa55551ad1097b31a
devesh01032008 <deveshsharma1009@gmail.com>
2021-11-12 15:32:09 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Motivational Quotes
+This website would display you Motivational quotes till you want them in english by different authors and famous personalities.
+
+## How to use?
+Swap and read motivational quotes as long as you want.
+
+## About Me
+I am Devesh Sharma from India. I am 13 years old student currently studying in JNV Panipat Class 9th B. I know several programming languages such as C, C++, Java, Python, HTMl, CSS and JavaScript. I have also worked with reactJs as framework. I am passionate about programming.
+
+## More Such Projects?
+I have GitHub profile where you can find such more projects.
---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+const loadQuote = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  let quote = document.querySelector("div.quote")
+  quote.innerHTML += `
+    <div class="container">
+        <div class="speech">
+          ${data.content}
+        </div>
+        <div class="author">
+           ${data.author}
+        </div>
+     </div>
+  `;
+}
+
+const addMoreQuotes = async () => {
+  const { scrollHeight, scrollTop, clientHeight } = document.documentElement;
+  if (scrollTop + clientHeight >= scrollHeight) {
+    for (let i = 0; i < 10; i++) {
+      loadQuote(url);
+    }
+  }
+}
+
+const toggleMenu = () => {
+  let header = document.querySelector("header");
+  let nav = document.querySelector("nav");
+  if (header.style.height == "25rem") {
+    header.style.height = "6rem";
+    nav.style.display = "none";
+
+  } else {
+    header.style.height = "25rem";
+    nav.style.display = "block";
+  }
+}
+
+const url = "https://api.quotable.io/random";
+const menu = document.querySelector("div.menu");
+
+window.addEventListener("load", () => {
+  for (let i = 0; i < 10; i++) {
+    loadQuote(url);
+  }
+})
+menu.addEventListener("click", toggleMenu);
+window.addEventListener("scroll", addMoreQuotes);
